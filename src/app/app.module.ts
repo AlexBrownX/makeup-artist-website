a56ee1cc8f79910d4038ff06c0100b5fcea5d268
Alex Brown <alexbrownx@gmail.com>
2017-10-14 15:20:24 +0100

    Adds about me page and router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from '../welcome-page/welcome-page.component';
+import { AboutMePageComponent } from '../about-me-page/about-me-page.component';
+import { NotFoundPageComponent } from '../not-found-page/not-found-page.component';
+import { NavigationComponent } from '../navigation/navigation.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'welcome',
+    component: WelcomePageComponent,
+    data: {
+      title: 'Jana Jurakove Wedding Makeup Artist'
+    }
+  }, {
+    path: 'about-me',
+    component: AboutMePageComponent,
+    data: {
+      title: 'About Jana Jurakove Wedding Makeup Artist'
+    }
+  }, {
+    path: '',
+    redirectTo: '/welcome',
+    pathMatch: 'full'
+  }, {
+    path: '**',
+    component: NotFoundPageComponent
+  }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    WelcomePageComponent,
+    AboutMePageComponent,
+    NotFoundPageComponent,
+    NavigationComponent
+],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes//,
+      // { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
