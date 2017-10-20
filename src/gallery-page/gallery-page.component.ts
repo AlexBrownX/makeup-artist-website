69273fb283b6f45b05523215e84929d854c5fa09
Alex Brown <alexbrownx@gmail.com>
2017-10-20 18:26:59 +0100

    Adds support to fetch multiple instgram images

--- src/gallery-page/gallery-page.component.ts
+++ src/gallery-page/gallery-page.component.ts
@@ -10,35 +10,73 @@ import { Observable } from 'rxjs/Rx';
 })
 export class GalleryPageComponent implements OnInit {
 
-  apiRoot = 'https://api.instagram.com/oembed/?url=http://instagr.am/p/BaUvvg0nOMI/?OMITSCRIPT=true&callback=JSONP_CALLBACK';
+  instagramRootUrl = 'https://api.instagram.com/oembed/?url=http://instagr.am/p/';
+  omitScript = '?OMITSCRIPT=true';
+  callbackFlag = '&callback=JSONP_CALLBACK';
+
+  galleryUris = [
+    'BaUvvg0nOMI'// , // Viera Wedding Looking Right
+    // 'BaUxkodHKSo', // Viera Closeup Face From Above
+    // 'BaXaGQinsGF', // Viera Laying down
+    // 'BaXcqmkHfe2' // Viera Chin and Dress
+  ];
+
   galleryHtml: String;
-  galleryThumbnailHtml: String;
-  thumbnailWidth: Number;
+  gallery = [];
+  tempGallery = [];
+  completeImageFetches = 0;
 
   constructor(private jsonp: Jsonp) {}
 
   ngOnInit() {
-    this.getGalleryImage().subscribe(data => {
-      this.galleryHtml = data['html'];
-      this.galleryThumbnailHtml = data['thumbnail_url'];
-      this.thumbnailWidth = data['thumbnail_width'];
-
-      this.triggerInstagram();
-    });
+    this.getInstagramImages();
   }
 
-  triggerInstagram() {
-    if (window['instgrm'] && window['instgrm'].Embeds) {
-      window['instgrm'].Embeds.process();
+  getInstagramImages() {
+    for (const imageUri of this.galleryUris) {
+      this.getGalleryImage(imageUri).subscribe(data => {
+        this.addImageToGallery(data);
+      });
     }
   }
 
-  getGalleryImage(): Observable<any[]> {
-    return this.jsonp.get(this.apiRoot)
+  addImageToGallery(imageData: any) {
+    this.tempGallery.push(imageData);
+    this.triggerIfAllComplete();
+  }
+
+  triggerIfAllComplete() {
+    this.completeImageFetches++;
+
+    if (this.completeImageFetches === this.galleryUris.length) {
+      this.gallery = this.tempGallery;
+      this.completeImageFetches = 0;
+      this.tempGallery = [];
+
+      this.triggerInstagramApi();
+    }
+  }
+
+  triggerInstagramApi() {
+    if (window['instgrm'] && window['instgrm'].Embeds) {
+      window['instgrm'].Embeds.process();
+    } else {
+      console.log('Tried to start Instagram embedded script but was not found.');
+    }
+  }
+
+  getGalleryImage(imageUri: string): Observable<any[]> {
+    const imageUrl = this.createImageUrl(imageUri);
+
+    return this.jsonp.get(imageUrl)
       .map(function(res: Response) {
           return res.json() || {};
       }).catch(function(error: any) {
         return Observable.throw(error);
       });
+  }
+
+  createImageUrl(imageUri: string) {
+    return this.instagramRootUrl + imageUri + this.omitScript + this.callbackFlag;
   }
 }
