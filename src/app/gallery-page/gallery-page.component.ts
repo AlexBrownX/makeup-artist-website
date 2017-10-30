9d0f1ca5b2d45f5e1ec5bb25d74cff1916cda934
Alex Brown <alexbrownx@gmail.com>
2017-10-30 22:12:03 +0000

    Disables gallery page animation

--- src/app/gallery-page/gallery-page.component.ts
+++ src/app/gallery-page/gallery-page.component.ts
@@ -1,3 +1,9 @@
+// TODO - Animation disabled on this page because the gallery viewer
+// is set to position: absolute. There is a quirk in Chromium when an ancestor
+// element is using transform (angular animations) then the absolute position
+// stops at that ancestor instead of the body.
+// https://bugs.chromium.org/p/chromium/issues/detail?id=20574
+
 import { Component,  AfterViewInit , HostBinding } from '@angular/core';
 import { slideInDownAnimation } from '../animations';
 import * as photoSwipeLoader from '../photoswipe-loader';
@@ -10,8 +16,8 @@ import * as globalVars from '../../globals';
   animations: [slideInDownAnimation]
 })
 export class GalleryPageComponent implements AfterViewInit {
-  @HostBinding('@routeAnimation') routeAnimation = true;
-  @HostBinding('style.display') display = 'block';
+//   @HostBinding('@routeAnimation') routeAnimation = true;
+//   @HostBinding('style.display') display = 'block';
 
   constructor() {}
 
