5a788c2d6de3fb04342cd02ab74cd093c7329432
Alex Brown <alexbrownx@gmail.com>
2018-04-25 20:32:25 +0100

    Adds new photos to gallery and sponsers flowers by elaine

--- src/globals.ts
+++ src/globals.ts
@@ -68,37 +68,147 @@ export const galleryDefinitions = [
     }, {
         mainImage: '9a.jpg',
         thumbnailImage: '9c.jpg',
-        caption: 'Luisa Wedding Photo 2017 1',
+        caption: 'Luisa Bride Photo 2017 1',
         size: '1024x683'
     }, {
         mainImage: '10a.jpg',
         thumbnailImage: '10c.jpg',
-        caption: 'Luisa Wedding Photo 2017 2',
+        caption: 'Luisa Bride Photo 2017 2',
         size: '1024x683'
     }, {
         mainImage: '11a.jpg',
         thumbnailImage: '11c.jpg',
-        caption: 'Luisa Wedding Photo 2017 3',
+        caption: 'Luisa Bride Photo 2017 3',
         size: '1024x683'
     }, {
         mainImage: '12a.jpg',
         thumbnailImage: '12c.jpg',
-        caption: 'Luisa Wedding Photo 2017 4',
+        caption: 'Luisa Bride Photo 2017 4',
         size: '681x1021'
     }, {
         mainImage: '13a.jpg',
         thumbnailImage: '13c.jpg',
-        caption: 'Luisa Wedding Photo 2017 5',
+        caption: 'Luisa Bride Photo 2017 5',
         size: '681x957'
     }, {
         mainImage: '14a.jpg',
         thumbnailImage: '14c.jpg',
-        caption: 'Luisa Wedding Photo 2017 6',
+        caption: 'Luisa Bride Photo 2017 6',
         size: '681x946'
     }, {
         mainImage: '15a.jpg',
         thumbnailImage: '15c.jpg',
-        caption: 'Viera Wedding Photo 2017 1',
+        caption: 'Viera Bride Photo 2017 1',
         size: '681x1021'
+    }, {
+        mainImage: '16a.jpg',
+        thumbnailImage: '16c.jpg',
+        caption: 'Charlotte Bride Photo 2018 1',
+        size: '681x1021'
+    }, {
+        mainImage: '17a.jpg',
+        thumbnailImage: '17c.jpg',
+        caption: 'Charlotte Bride Photo 2018 2',
+        size: '681x1021'
+    }, {
+        mainImage: '18a.jpg',
+        thumbnailImage: '18c.jpg',
+        caption: 'Charlotte Bride Photo 2018 3',
+        size: '681x1021'
+    }, {
+        mainImage: '19a.jpg',
+        thumbnailImage: '19c.jpg',
+        caption: 'Charlotte Bride Photo 2018 4',
+        size: '681x1021'
+    }, {
+        mainImage: '20a.jpg',
+        thumbnailImage: '20c.jpg',
+        caption: 'Charlotte Bride Photo 2018 5',
+        size: '681x1021'
+    }, {
+        mainImage: '21a.jpg',
+        thumbnailImage: '21c.jpg',
+        caption: 'Charlotte Bride Photo 2018 6',
+        size: '681x1021'
+    }, {
+        mainImage: '22a.jpg',
+        thumbnailImage: '22c.jpg',
+        caption: 'Charlotte Bride Photo 2018 7',
+        size: '681x1021'
+    }, {
+        mainImage: '23a.jpg',
+        thumbnailImage: '23c.jpg',
+        caption: 'Ines Bride Photo 2018 1',
+        size: '681x1021'
+    }, {
+        mainImage: '24a.jpg',
+        thumbnailImage: '24c.jpg',
+        caption: 'Ines Bride Photo 2018 2',
+        size: '681x1021'
+    }, {
+        mainImage: '25a.jpg',
+        thumbnailImage: '25c.jpg',
+        caption: 'Ines Bride Photo 2018 3',
+        size: '681x1022'
+    }, {
+        mainImage: '26a.jpg',
+        thumbnailImage: '26c.jpg',
+        caption: 'Ines Bride Photo 2018 4',
+        size: '681x1021'
+    }, {
+        mainImage: '27a.jpg',
+        thumbnailImage: '27c.jpg',
+        caption: 'Ines Bride Photo 2018 5',
+        size: '524x536'
+    }, {
+        mainImage: '28a.jpg',
+        thumbnailImage: '28c.jpg',
+        caption: 'Bohemian Bride Photo 2018 1',
+        size: '681x1008'
+    }, {
+        mainImage: '29a.jpg',
+        thumbnailImage: '29c.jpg',
+        caption: 'Bohemian Bride Photo 2018 2',
+        size: '681x1011'
+    }, {
+        mainImage: '30a.jpg',
+        thumbnailImage: '30c.jpg',
+        caption: 'Bohemian Bride Photo 2018 3',
+        size: '681x1015'
+    }, {
+        mainImage: '31a.jpg',
+        thumbnailImage: '31c.jpg',
+        caption: 'Bohemian Bride Photo 2018 4',
+        size: '681x1010'
+    }, {
+        mainImage: '32a.jpg',
+        thumbnailImage: '32c.jpg',
+        caption: 'Bohemian Bride Photo 2018 5',
+        size: '681x1010'
+    }, {
+        mainImage: '33a.jpg',
+        thumbnailImage: '33c.jpg',
+        caption: 'Bohemian Bride Photo 2018 6',
+        size: '681x1012'
+    }, {
+        mainImage: '34a.jpg',
+        thumbnailImage: '34c.jpg',
+        caption: 'Bohemian Bride Photo 2018 7',
+        size: '1024x678'
+    }, {
+        mainImage: '35a.jpg',
+        thumbnailImage: '35c.jpg',
+        caption: 'Bohemian Bride Photo 2018 8',
+        size: '1024x675'
+    }, {
+        mainImage: '36a.jpg',
+        thumbnailImage: '36c.jpg',
+        caption: 'Bohemian Bride Photo 2018 9',
+        size: '1024x672'
+    }, {
+        mainImage: '37a.jpg',
+        thumbnailImage: '37c.jpg',
+        caption: 'Bohemian Bride Photo 2018 10',
+        size: '1024x674'
     }
 ];
