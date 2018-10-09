6888ac496a29a831f9301a8044fd904ba02d489e
Alex Brown <alexbrownx@gmail.com>
2018-10-09 20:06:54 +0100

    Whitespace

--- src/app/SafePipe.pipe.spec.ts
+++ src/app/SafePipe.pipe.spec.ts
@@ -7,7 +7,7 @@
 
 // describe('Pipe: SafePipee', () => {
 //   it('create an instance', () => {
-//     const pipe = new SafePipe(); 
+//     const pipe = new SafePipe();
 //     expect(pipe).toBeTruthy();
 //   });
 // });
