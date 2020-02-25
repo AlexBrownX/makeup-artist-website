9b36846e9bf7232b2a2094e751437862d52d5bf4
Alex Brown <alexbrownx@gmail.com>
2020-02-25 20:33:16 +0000

    Adds new wedding photos to gallery

--- src/globals.ts
+++ src/globals.ts
@@ -26,126 +26,77 @@ export interface GalleryDefinition {
 
 export const weddingGalleryDefinitions = [
   {
-    mainImage: '1a.jpg',
-    thumbnailImage: '1c.jpg',
-    caption: 'C & V Wedding Photo 2017 1',
-    size: '1024x681'
-  }, {
-    mainImage: '2a.jpg',
-    thumbnailImage: '2c.jpg',
-    caption: 'C & V Wedding Photo 2017 2',
-    size: '1024x681'
-  }, {
-    mainImage: '3a.jpg',
-    thumbnailImage: '3c.jpg',
-    caption: 'C & V Wedding Photo 2017 3',
-    size: '1024x681'
-  }, {
-    mainImage: '4a.jpg',
-    thumbnailImage: '4c.jpg',
-    caption: 'C & V Wedding Photo 2017 4',
-    size: '1024x681'
-  }, {
-    mainImage: '5a.jpg',
-    thumbnailImage: '5c.jpg',
-    caption: 'C & V Wedding Photo 2017 5',
-    size: '1024x681'
-  }, {
-    mainImage: '6a.jpg',
-    thumbnailImage: '6c.jpg',
-    caption: 'C & V Wedding Photo 2017 6',
-    size: '1024x681'
-  }, {
-    mainImage: '7a.jpg',
-    thumbnailImage: '7c.jpg',
-    caption: 'C & V Wedding Photo 2017 7',
-    size: '1024x681'
-  }, {
-    mainImage: '8a.jpg',
-    thumbnailImage: '8c.jpg',
-    caption: 'C & V Wedding Photo 2017 8',
-    size: '1024x681'
-  }, {
     mainImage: '43a.jpg',
     thumbnailImage: '43c.jpg',
-    caption: 'C & V Wedding Photo 2017 9',
+    caption: 'C & V Wedding Photo 2017 1',
     size: '681x1024'
   }, {
     mainImage: '44a.jpg',
     thumbnailImage: '44c.jpg',
-    caption: 'C & V Wedding Photo 2017 10',
+    caption: 'C & V Wedding Photo 2017 2',
     size: '681x1024'
   }, {
     mainImage: '45a.jpg',
     thumbnailImage: '45c.jpg',
-    caption: 'C & V Wedding Photo 2017 11',
+    caption: 'C & V Wedding Photo 2017 3',
     size: '681x1024'
   }, {
     mainImage: '46a.jpg',
     thumbnailImage: '46c.jpg',
-    caption: 'C & V Wedding Photo 2017 12',
+    caption: 'C & V Wedding Photo 2017 4',
     size: '681x1024'
-  }, {
-    mainImage: '47a.jpg',
-    thumbnailImage: '47c.jpg',
-    caption: 'C & V Wedding Photo 2017 13',
-    size: '1024x681'
-  }, {
-    mainImage: '48a.jpg',
-    thumbnailImage: '48c.jpg',
-    caption: 'C & V Wedding Photo 2017 14',
-    size: '1024x681'
-  }, {
-    mainImage: '49a.jpg',
-    thumbnailImage: '49c.jpg',
-    caption: 'C & V Wedding Photo 2017 15',
-    size: '1024x681'
-  }, {
-    mainImage: '50a.jpg',
-    thumbnailImage: '50c.jpg',
-    caption: 'S & T Wedding Photo 2018 1',
-    size: '1024x681'
-  }, {
-    mainImage: '51a.jpg',
-    thumbnailImage: '51c.jpg',
-    caption: 'S & T Wedding Photo 2018 2',
-    size: '1024x681'
-  }, {
-    mainImage: '52a.jpg',
-    thumbnailImage: '52c.jpg',
-    caption: 'S & T Wedding Photo 2018 3',
-    size: '1024x681'
   }, {
     mainImage: '53a.jpg',
     thumbnailImage: '53c.jpg',
-    caption: 'S & T Wedding Photo 2018 4',
+    caption: 'S & T Wedding Photo 2018 1',
     size: '681x1024'
   }, {
     mainImage: '54a.jpg',
     thumbnailImage: '54c.jpg',
-    caption: 'S & T Wedding Photo 2018 5',
-    size: '681x1024'
-  }, {
-    mainImage: '55a.jpg',
-    thumbnailImage: '55c.jpg',
-    caption: 'S & T Wedding Photo 2018 6',
+    caption: 'S & T Wedding Photo 2018 2',
     size: '681x1024'
   }, {
     mainImage: '56a.jpg',
     thumbnailImage: '56c.jpg',
-    caption: 'S & T Wedding Photo 2018 7',
+    caption: 'S & T Wedding Photo 2018 3',
     size: '681x1024'
   }, {
     mainImage: '57a.jpg',
     thumbnailImage: '57c.jpg',
-    caption: 'S & T Wedding Photo 2018 8',
+    caption: 'S & T Wedding Photo 2018 4',
     size: '681x1024'
   }, {
-    mainImage: '58a.jpg',
-    thumbnailImage: '58c.jpg',
-    caption: 'S & T Wedding Photo 2018 9',
-    size: '681x1024'
+    mainImage: '1a.jpg',
+    thumbnailImage: '1c.jpg',
+    caption: 'S & J Wedding Photo 2019 1',
+    size: '1024x1534'
+  }, {
+    mainImage: '2a.jpg',
+    thumbnailImage: '2c.jpg',
+    caption: 'S & J Wedding Photo 2019 2',
+    size: '1024x1536'
+  }, {
+    mainImage: '3a.jpg',
+    thumbnailImage: '3c.jpg',
+    caption: 'S & J Wedding Photo 2019 3',
+    size: '1024x1536'
+  }, {
+    mainImage: '4a.jpg',
+    thumbnailImage: '4c.jpg',
+    caption: 'S & J Wedding Photo 2019 4',
+    size: '1024x1536'
+  }, {
+    mainImage: '5a.jpg',
+    thumbnailImage: '5c.jpg',
+    caption: 'S & J Wedding Photo 2019 5',
+    size: '1024x1536'
+  }, {
+    mainImage: '6a.jpg',
+    thumbnailImage: '6c.jpg',
+    caption: 'S & J Wedding Photo 2019 6',
+    size: '1024x1536'
   }
+
 ];
 
 export const bridesGalleryDefinitions = [
